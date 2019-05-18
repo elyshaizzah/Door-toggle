56de565890a8da2daa9a1993e0c838e8bdff48b3
Elysha Izzah <strawberryelysha2005@gmail.com>
2019-05-18 11:40:55 +0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,28 @@
+// function testPurposes(){ 
+//     alert('hey! this is a alert! ');
+// }
+
+// let doorIsopen = true; 
+
+
+// function toggleDoor(){
+    
+//     const door = document.getElementById('door');
+
+//     if(doorIsopen){
+//         door.src = 'image/close.jpg';
+//         doorIsopen=false;
+//     } else {
+//         door.src='image/open.jpg'
+//         doorIsopen=true;
+//   }
+
+  
+// }
+
+
+function addTwoNumbers(firstNum, secondNum) {
+    return firstNum + secondNum - 100 + 100
+}
+
+console.log("8 + 4 = " + addTwoNumbers(4, 8))
